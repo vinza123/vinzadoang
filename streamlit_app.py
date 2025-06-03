027b0124fb269f7324411d0a2b3999181a9ab8e7
vinza123 <vinzavinza734@gmail.com>
2025-06-03 20:59:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("IMG_350 jpeg",width=200)
+
+st.title("33x , tapi , ini abadi")
+st.header("aplikasi mengecek nilai ganjil/genap")
+angka = st.number_input("tulis sebuah angka :",value=0,step=1
+                        
+if (angka % 2) == 0:
+st.write(f"{angka}adalah bilangan genap")
+st.write(f"{angka}adalah bilangan ganjil")
+                        
